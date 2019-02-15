52784bec858b211fd930d3a90fd3fbc9bdc78f1d
Sakthi <narayanansakthi1992@gmail.com>
2019-02-15 14:29:15 +0530

    Added in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from 'src/app/Header/Header.component';
+import { InterestComponent } from './interest/interest.component';
+import { PersonalComponent } from './personal/personal.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    InterestComponent,
+    PersonalComponent,
+    ProjectsComponent,
+    ContactsComponent,
+    SocialComponent,
   ],
   imports: [
     BrowserModule
